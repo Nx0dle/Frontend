0a2a7c2803987689ae256e1676c60545d7ced6d7
Nx0dle <ramenkeebs@proton.me>
2024-03-23 20:55:04 +0100

    navbar mobile

--- Doya/dist/js/all.js
+++ Doya/dist/js/all.js
@@ -1,1 +1,22 @@
-console.log("test");
+const menuMobile = document.querySelector('#menu-mobile')
+const overlay = document.querySelector('#overlay')
+
+const show = (e) => {
+    return function() {
+        e.classList.add('visible')
+        e.classList.remove('hidden')
+    }
+}
+
+const hide = (e) => {
+    return function() {
+        e.classList.add('hidden')
+        e.classList.remove('visible')
+    }
+}
+
+document.querySelector('#toggle-mobile').addEventListener('click', show(overlay))
+document.querySelector('#toggle-mobile').addEventListener('click', show(menuMobile))
+
+document.querySelector('#hide-menu').addEventListener('click', hide(overlay))
+document.querySelector('#hide-menu').addEventListener('click', hide(menuMobile))
